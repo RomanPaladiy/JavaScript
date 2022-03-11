4b3033edc48e70cc00fa6aef16dc5bd3a03131f8
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-03-10 16:02:15 -0800

    Assignment submission

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,15 @@
+//This code is adding 3 and 15 to create the sum of 18, it is also utilizing the document.write method.
+document.write(3+15);
 
-document.write('Hello,World!');
+//This is utilizing the window.alert method.
+window.alert('This is an alert!');
+
+//Contatenation.
+document.write("The more you practice writing Javascript code,"
++ " the easier future assignments will be.");
+
+//Expression 
+document.write(5*25);
+
+//Font-color, On HTMl File
+
