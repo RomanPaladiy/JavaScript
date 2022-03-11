ec8aee4f7cf785e8defcc72a48663717f352e7dc
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-03-11 13:54:34 -0800

    Comments

--- Basic_JavaScript_Projects/Project2_Functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_Functions/JS/main.js
@@ -1,3 +1,5 @@
+
+// This is the 2 strings that are being concatenated together
 function myFunction() {
     var sentence = "I am learning and getting";
     sentence += " alot from this bootcamp!";
