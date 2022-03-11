bb3c876ecebfd93d932dd5897f1418fa69175a77
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-03-11 13:53:06 -0800

    Updated Work

---
+++ Basic_JavaScript_Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "I am learning and getting";
+    sentence += " alot from this bootcamp!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
