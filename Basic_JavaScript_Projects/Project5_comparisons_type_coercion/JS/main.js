23ba7eac6d94a4a091cfc121faf990da9fc19024
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-03-14 11:32:04 -0700

    Submission

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,48 @@
+document.write("10" + 5);
+
+document.write(typeof "Ten");
+
+function my_Function()  {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(10 > 2);
+
+document.write(10 < 2);
+
+console.log(2 + 2);
+
+console.log( 2 > 8);
+
+document.write(10 == 10);
+
+document.write(3 == 11);
+
+x = 15;
+y = 15;
+document.write(x === y);
+
+x = 82;
+y= "82";
+document.write(x == y);
+
+A = "Magnus";
+B = "Magnus";
+document.write(A === B);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function()  {
+    document.getElementById("Not").innerHTML = !(20 > 10) ;
+}
+
