98959e84423b52043c8a6a6d64bf3be580ffe64c
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-03-14 23:00:27 -0700

    Updated code

---
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,20 @@
+var x = 10;
+function Add_numbers_1() {
+    document.write(20 + x + "<br>");
+}
+function Add_numbers_2() {
+    document.write(x + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+
+function Add_numbers_1() {
+    var x = 10;
+    document.write(20 + x + "<br>");
+}
+function Add_numbers_2() {
+    document.write(x + 100);
+    }
+Add_numbers_1();
+Add_numbers_2();
