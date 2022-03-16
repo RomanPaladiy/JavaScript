79acf1e6c1061b0b178ff797e226a101068cb09f
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-03-16 08:59:09 -0700

    Submission

--- Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -7,3 +7,42 @@ function full_sentence() {
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
 
+function slice_method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function to_uppercase() {
+    var text = "Hello World!";
+    var result = text.toUpperCase();
+    document.getElementById("Upper").innerHTML = result;
+}
+
+function to_search() {
+    var text = "This is a white house"
+    var position = text.search("white");
+    document.getElementById("Search").innerHTML = position;
+}
+
+function string_method() {
+    var x =182;
+    document.getElementById("Numbers_to_string").innerHTML = x.toString();
+}
+
+function Precision_Method() {
+    var x = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = x.toPrecision(10);
+}
+
+function to_fixed() {
+    var num = 5.5684;
+    var n = num.toFixed();
+    document.getElementById("fixed").innerHTML = n;
+}
+
+function Value_Of() {
+    var text = "Hello World!";
+    var result = text.valueOf();
+    document.getElementById("Value").innerHTML = result;
+}
