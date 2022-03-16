67f4ebc25917e049d445733a328822aeea7c2e18
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-03-15 18:12:36 -0700

    Submission

--- Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -18,3 +18,25 @@ function Add_numbers_2() {
     }
 Add_numbers_1();
 Add_numbers_2();
+//Greeting message 
+function get_Date() {
+    if (new Date().getHours() <18) {
+    document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+
+if (5 < 13) {
+    document.write("The number on the left is smaller than the number on the right. ")
+}
+// Else/If function practice
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old enough to vite!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
