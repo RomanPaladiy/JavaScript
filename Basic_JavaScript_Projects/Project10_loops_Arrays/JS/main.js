0240306f75794c8a8bd26144d539c24972d18238
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-03-18 21:14:08 -0700

    Updated code for submission

--- Basic_JavaScript_Projects/Project10_loops_Arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_Arrays/JS/main.js
@@ -1,3 +1,4 @@
+//Counts to ten
 function count_To_Ten() {
     var digit = "";
     var x = 1;
@@ -7,7 +8,7 @@ function count_To_Ten() {
     }
     document.getElementById("Counting_to_Ten").innerHTML = digit
 }
-
+//Specifies the length of the string.
 function String_Length() {
     var text = "Hello people of the world.";
     var Length = text.length;
@@ -23,3 +24,49 @@ function for_Loop() {
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
+
+
+function cat_pics() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " + Cat_Picture[3] + ".";
+}
+
+function constant_function() {
+    const Musical_Instrument = {type:"Drum", brand:"Yamaha", color:"brown"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$1320";
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
+
+let car = {
+  make: "Tesla ",
+  model: "Model S ",
+  year: "2022 ",
+  color: "Black ",
+  description : function() {
+    return "The car is a " + this.year + this.color + this.make + this.model;
+  }
+    
+}
+
+function break_loop() {
+     let text = "";
+     for (let i =0; i < 10; i++) {
+       if (i === 3) { break; }
+       text += "The number is " + i + "<br>";
+       document.getElementById("Break").innerHTML = text;
+    }
+  }
+
+function continue_loop() {
+    let text = "";
+    for (let i =0; i < 10; i++) {
+      if (i === 3) { continue; }
+      text += "The number is " + i + "<br>";
+      document.getElementById("Continue").innerHTML = text;
+   }
+ }
